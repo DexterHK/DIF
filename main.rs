b34486f86989883ae930329b00354333616f9cbe
Baibars <dexterhk01@gmail.com>
2024-04-25 14:07:16 +0200

    test

--- main.rs
+++ main.rs
@@ -1,15 +1,15 @@
 // Function to multiply two numbers
 fn multiply(x: i32, y: i32) -> i32 {
-    x*y
+    x * y
 }
 
 fn main() {
-    let num1=5;
-    let num2=10;
+    let num1 = 5;
+    let num2 = 10;
 
     // Call the multiply function and store the result
-    let result=multiply(num1,num2) ;
- 
+    let result = multiply(num1, num2);
+
     // Print the result
-    println!("Result of { } * {} = {}", num1, num2, result);
+    println!("Result of {} * {} = {}", num1, num2, result);
 }
